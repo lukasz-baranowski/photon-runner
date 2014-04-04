d2ac65563e09e9739c891a62f34cbb81627a32bb
Jaroslaw Jarmolowicz <jaroslaw.jarmolowicz@tomtom.com>
2014-04-04 17:50:43 +0200

    adding version to match: 17.4.4.3

--- src/main/java/com/tomtom/photon/runner/threads/SendRunner.java
+++ src/main/java/com/tomtom/photon/runner/threads/SendRunner.java
@@ -27,171 +27,170 @@ import com.tomtom.photon.tools.zonemaker.ZoneMaker;
 
 public class SendRunner implements Callable<Void> {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(SendRunner.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(SendRunner.class);
 
-    private final ZoneMakerConf zoneMakerConf;
-    private final FetchRunner fetchTask;
+	private final ZoneMakerConf zoneMakerConf;
+	private final FetchRunner fetchTask;
 
-    private final File fetchOut;
-    private final File sentOut;
+	private final File fetchOut;
+	private final File sentOut;
 
-    protected boolean finished = false;
+	protected boolean finished = false;
 
-    private final File staging;
+	private final File staging;
 
-    public SendRunner(ZoneMakerConf zoneMakerCnf, FetchRunner fetchTask) {
-        zoneMakerConf = zoneMakerCnf;
-        fetchOut = new File(zoneMakerCnf.getOut(), PhotonRunner.FETCHED_DIR);
-        sentOut = new File(zoneMakerCnf.getOut(), PhotonRunner.SENT_DIR);
-        staging = new File(zoneMakerCnf.getOut(), "tmp");
-        sentOut.mkdirs();
-        staging.mkdirs();
-        this.fetchTask = fetchTask;
-    }
+	public SendRunner(ZoneMakerConf zoneMakerCnf, FetchRunner fetchTask) {
+		zoneMakerConf = zoneMakerCnf;
+		fetchOut = new File(zoneMakerCnf.getOut(), PhotonRunner.FETCHED_DIR);
+		sentOut = new File(zoneMakerCnf.getOut(), PhotonRunner.SENT_DIR);
+		staging = new File(zoneMakerCnf.getOut(), "tmp");
+		sentOut.mkdirs();
+		staging.mkdirs();
+		this.fetchTask = fetchTask;
+	}
 
-    @Override
-    public Void call() throws Exception {
-        try {
-            runSend();
-            finished = true;
-        } catch (Exception e) {
-            LOGGER.error(e.getMessage(), e);
-            throw e;
-        }
-        return null;
-    }
+	@Override
+	public Void call() throws Exception {
+		try {
+			runSend();
+			finished = true;
+		} catch (Exception e) {
+			LOGGER.error(e.getMessage(), e);
+			throw e;
+		}
+		return null;
+	}
 
-    private void runSend() throws Exception {
-        fetchOut.mkdirs();
-        while (true) {
-            Optional<File> nextFileToSend = getNextFileToSend();
+	private void runSend() throws Exception {
+		fetchOut.mkdirs();
+		while (true) {
+			Optional<File> nextFileToSend = getNextFileToSend();
 
-            if (fetchTask.finished && !nextFileToSend.isPresent()) {
-                break;
-            }
+			if (fetchTask.finished && !nextFileToSend.isPresent()) {
+				break;
+			}
 
-            if (nextFileToSend.isPresent()) {
-                File file = nextFileToSend.get();
-                File toBeSent = moveJsonFileFromFetchedToBeSent(file);
-                File doneMarker = new File(file.getAbsolutePath() + ".done");
-                if (!doneMarker.exists()) {
-                    String name = file.getName();
-                    Params p = createParamsFile(file.getParentFile(), name);
-                    LOGGER.info("Sending " + name);
-                    runZoneMaker(p);
-                    LOGGER.info("Sent to zoning " + name);
-                    moveJsonFileFromStagingToSent(file, toBeSent);
-                    doneMarker.createNewFile();
-                }
-            } else {
-                TimeUnit.SECONDS.sleep(2);
-            }
+			if (nextFileToSend.isPresent()) {
+				File file = nextFileToSend.get();
+				File toBeSent = moveJsonFileFromFetchedToBeSent(file);
+				File doneMarker = new File(file.getAbsolutePath() + ".done");
+				if (!doneMarker.exists()) {
+					String name = file.getName();
+					Params p = createParamsFile(file.getParentFile(), name);
+					LOGGER.info("Sending " + name);
+					runZoneMaker(p);
+					LOGGER.info("Sent to zoning " + name);
+					moveJsonFileFromStagingToSent(file, toBeSent);
+					doneMarker.createNewFile();
+				}
+			} else {
+				TimeUnit.SECONDS.sleep(2);
+			}
 
-        }
-        LOGGER.info("Finished");
-    }
+		}
+		LOGGER.info("Finished");
+	}
 
-    private void moveJsonFileFromStagingToSent(File file, File toBeSent) throws IOException {
-        File destDir = new File(sentOut, file.getParentFile().getName());
-        destDir.mkdirs();
-        File dest = new File(destDir, toBeSent.getName());
-        toBeSent.renameTo(dest);
+	private void moveJsonFileFromStagingToSent(File file, File toBeSent) throws IOException {
+		File destDir = new File(sentOut, file.getParentFile().getName());
+		destDir.mkdirs();
+		File dest = new File(destDir, toBeSent.getName());
+		toBeSent.renameTo(dest);
 
-        copyPropertiesFile(file.getParentFile());
-    }
+		copyPropertiesFile(file.getParentFile());
+	}
 
-    private File moveJsonFileFromFetchedToBeSent(File nextFileToSend) throws IOException {
-        File dest = new File(staging, nextFileToSend.getName());
-        IOUtil.rmDir(staging);
-        staging.mkdirs();
+	private File moveJsonFileFromFetchedToBeSent(File nextFileToSend) throws IOException {
+		File dest = new File(staging, nextFileToSend.getName());
+		IOUtil.rmDir(staging);
+		staging.mkdirs();
 
-        BufferedReader reader = new BufferedReader(new FileReader(nextFileToSend));
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(dest));
-            try {
-                String baseContent = IOUtils.toString(reader);
-                String contVersion = Integer.toString(nextFileToSend.getParentFile().getName().charAt(0) - 'A');
-                String replaced =
-                    baseContent.replace("\"version\" : \"", "\"version\" : \"" + contVersion);
-                IOUtils.write(replaced, writer);
-            } finally {
-                IOUtils.closeQuietly(writer);
-            }
-        } finally {
-            IOUtils.closeQuietly(reader);
-        }
-        return dest;
-    }
+		BufferedReader reader = new BufferedReader(new FileReader(nextFileToSend));
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(dest));
+			try {
+				String baseContent = IOUtils.toString(reader);
+				String contVersion = Integer.toString(nextFileToSend.getParentFile().getName().charAt(0) - 'A') + ".";
+				String replaced = baseContent.replace("\"version\" : \"", "\"version\" : \"" + contVersion);
+				IOUtils.write(replaced, writer);
+			} finally {
+				IOUtils.closeQuietly(writer);
+			}
+		} finally {
+			IOUtils.closeQuietly(reader);
+		}
+		return dest;
+	}
 
-    private Optional<File> getNextFileToSend() throws IOException {
-        for (File continentFetchOut : fetchOut.listFiles()) {
-            if (continentFetchOut.exists() && continentFetchOut.isDirectory()) {
-                Optional<File> fileToSend = getNextFileToSend(continentFetchOut);
-                if (fileToSend.isPresent()) {
-                    return fileToSend;
-                }
-            }
-        }
-        return Optional.absent();
-    }
+	private Optional<File> getNextFileToSend() throws IOException {
+		for (File continentFetchOut : fetchOut.listFiles()) {
+			if (continentFetchOut.exists() && continentFetchOut.isDirectory()) {
+				Optional<File> fileToSend = getNextFileToSend(continentFetchOut);
+				if (fileToSend.isPresent()) {
+					return fileToSend;
+				}
+			}
+		}
+		return Optional.absent();
+	}
 
-    private void copyPropertiesFile(File continentFetchOut) throws IOException {
-        File continentSentOut = new File(sentOut, continentFetchOut.getName());
-        File properties = new File(continentFetchOut, "$_$" + continentFetchOut.getName() + ".properties");
-        File copied = new File(continentSentOut, properties.getName());
-        if (!copied.exists()) {
-            LOGGER.info("Copying to sent " + properties.getName());
-            Files.copy(properties, copied);
-        }
-    }
+	private void copyPropertiesFile(File continentFetchOut) throws IOException {
+		File continentSentOut = new File(sentOut, continentFetchOut.getName());
+		File properties = new File(continentFetchOut, "$_$" + continentFetchOut.getName() + ".properties");
+		File copied = new File(continentSentOut, properties.getName());
+		if (!copied.exists()) {
+			LOGGER.info("Copying to sent " + properties.getName());
+			Files.copy(properties, copied);
+		}
+	}
 
-    private void runZoneMaker(Params p) {
-        ZoneMaker zm = new ZoneMaker(p);
-        zm.run();
-    }
+	private void runZoneMaker(Params p) {
+		ZoneMaker zm = new ZoneMaker(p);
+		zm.run();
+	}
 
-    private Optional<File> getNextFileToSend(File directory) {
-        File[] files = directory.listFiles(new FilenameFilter() {
+	private Optional<File> getNextFileToSend(File directory) {
+		File[] files = directory.listFiles(new FilenameFilter() {
 
-            @Override
-            public boolean accept(File dir, String name) {
-                if (name.endsWith(".json") && !name.startsWith("$")) {
-                    return true;
-                }
-                return false;
-            }
-        });
+			@Override
+			public boolean accept(File dir, String name) {
+				if (name.endsWith(".json") && !name.startsWith("$")) {
+					return true;
+				}
+				return false;
+			}
+		});
 
-        if (files.length == 0) {
-            return Optional.absent();
-        } else {
-            return Optional.of(files[0]);
-        }
-    }
+		if (files.length == 0) {
+			return Optional.absent();
+		} else {
+			return Optional.of(files[0]);
+		}
+	}
 
-    private Params createParamsFile(File continentSentDir, String name) {
-        Params p = zoneMakerConf.createBasicParams(Params.WORK_MODE.SEND);
-        Properties props = SendRunner.readPropertiesFile(continentSentDir);
-        p.setOutputDir(staging.getAbsolutePath());
-        p.setRegionName(props.getProperty("name"));
-        p.setRegionVersion(props.getProperty("version"));
-        if (ZoneMakerConf.ADM_MODE_CONTINENTS.contains(props.getProperty("name", ""))) {
-            p.setAdministrativeLevel(Params.ADMINISTRATIVE_LEVEL.ORDER1);
-        }
-        return p;
-    }
+	private Params createParamsFile(File continentSentDir, String name) {
+		Params p = zoneMakerConf.createBasicParams(Params.WORK_MODE.SEND);
+		Properties props = SendRunner.readPropertiesFile(continentSentDir);
+		p.setOutputDir(staging.getAbsolutePath());
+		p.setRegionName(props.getProperty("name"));
+		p.setRegionVersion(props.getProperty("version"));
+		if (ZoneMakerConf.ADM_MODE_CONTINENTS.contains(props.getProperty("name", ""))) {
+			p.setAdministrativeLevel(Params.ADMINISTRATIVE_LEVEL.ORDER1);
+		}
+		return p;
+	}
 
-    public static Properties readPropertiesFile(File continentSentDir) {
-        final String continentName = continentSentDir.getName();
-        final Properties props = new Properties();
-        try {
-            File f = new File(continentSentDir, "$_$" + continentName + ".properties");
-            InputStream is = new FileInputStream(f);
-            props.load(is);
-        } catch (Exception e) {
-            LOGGER.error(e.getMessage(), e);
-        }
-        return props;
-    }
+	public static Properties readPropertiesFile(File continentSentDir) {
+		final String continentName = continentSentDir.getName();
+		final Properties props = new Properties();
+		try {
+			File f = new File(continentSentDir, "$_$" + continentName + ".properties");
+			InputStream is = new FileInputStream(f);
+			props.load(is);
+		} catch (Exception e) {
+			LOGGER.error(e.getMessage(), e);
+		}
+		return props;
+	}
 
 }
